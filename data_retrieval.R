52a038dc8d7c31a8da94c2fb9fc712a1d67bca7e
rhidmoore <rmoore84@vols.utk.edu>
2019-11-12 15:04:13 -0500

    Improved column filtering and removed total concentrations

--- data_retrieval.R
+++ data_retrieval.R
@@ -1,31 +1,45 @@
+## This script uses the dataRetrieval package created by the USGS for retrieva
+## of their water quality data. The data frame has been summarized to only
+## contain necessary information, and has removed any data with "total" rather
+## than "dissolved" concentrations
+
 library(dataRetrieval) # package that reads and downloads water data from USGS Water Quality Portal website
-library(tidyverse)
+library(tidyverse) # perform dplyr piping operations
 
 
-# read in desired data: Inorganic cations/anions in stream water, on Big Island, Hawaii
-water_data <- readWQPdata(statecode ="Hawaii", 
+# read in desired data: Major inorganic ions in stream and well water, 
+# on the Big Island, Hawaii
+siteData <- readWQPdata(statecode ="Hawaii", 
                           countycode = "Hawaii County", 
                           SiteType = c("Stream", "Spring", "Well"),
                           SampleMedia = "Water",
                           CharacteristicType = c("Inorganics, Major, Metals", 
                                                  "Inorganics, Major, Non-metals"))
 
-# data fram with site info: name, lat/long, drainage area, etc
-siteInfo <- attr(water_data, "siteInfo")
+# data frame with site info: name, lat/long, drainage area, etc
+siteInfo <- attr(siteData, "siteInfo")
 
-Summary <- water_data %>%
+# combine both tables for one comprehensive table of data + site ID
+Hawaii_water_data <- siteData %>%
+  left_join(siteInfo, by = "MonitoringLocationIdentifier") %>%
   select("MonitoringLocationIdentifier",
-           "ActivityMediaSubdivisionName", 
-           "ActivityStartDate", 
-           "ActivityStartTime.Time",
-           "CharacteristicName",
-           "ResultSampleFractionText",
-           "ResultMeasureValue",
-           "ResultMeasure.MeasureUnitCode",
-           "USGSPCode") %>%
-  filter(ResultMeasure.MeasureUnitCode %in% c("mg/l", 'mg/l CaCO3', 'mg/l as Na')) %>%
+         "MonitoringLocationName",
+         "MonitoringLocationTypeName",
+         "ActivityStartDate", 
+         "ActivityStartTime.Time",
+         "CharacteristicName",
+         "ResultSampleFractionText",
+         "ResultMeasureValue",
+         "ResultMeasure.MeasureUnitCode",
+         "USGSPCode",
+         "WellDepthMeasure.MeasureValue",
+         "WellDepthMeasure.MeasureUnitCode",
+         "LatitudeMeasure",
+         "LongitudeMeasure") %>%
+  filter(ResultMeasure.MeasureUnitCode %in% c("mg/l", 'mg/l CaCO3', 'mg/l as Na'), 
+         ResultSampleFractionText != "Total") %>%
   distinct() %>%
-  group_by(MonitoringLocationIdentifier) %>%
-  left_join(siteInfo, by = "MonitoringLocationIdentifier")
+  group_by(MonitoringLocationIdentifier)
+
 
 
