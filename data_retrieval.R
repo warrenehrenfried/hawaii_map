68df517cd46ccc5e5b7fa90d246f7e9b83b4765e
rhidmoore <rmoore84@vols.utk.edu>
2019-11-12 14:03:14 -0500

    Finalized WQ data frame; removed extraneous info from data frame

--- data_retrieval.R
+++ data_retrieval.R
@@ -1,29 +1,31 @@
 library(dataRetrieval) # package that reads and downloads water data from USGS Water Quality Portal website
-library(dplyr)
+library(tidyverse)
 
 
 # read in desired data: Inorganic cations/anions in stream water, on Big Island, Hawaii
 water_data <- readWQPdata(statecode ="Hawaii", 
                           countycode = "Hawaii County", 
-                          SiteType = "Stream",
+                          SiteType = c("Stream", "Spring", "Well"),
                           SampleMedia = "Water",
-                          CharacteristicType ="Inorganics, Major, Metals")
-
-unique(water_data$ResultMeasure.MeasureUnitCode)
+                          CharacteristicType = c("Inorganics, Major, Metals", 
+                                                 "Inorganics, Major, Non-metals"))
 
 # data fram with site info: name, lat/long, drainage area, etc
 siteInfo <- attr(water_data, "siteInfo")
 
 Summary <- water_data %>%
-  filter(ResultMeasure.MeasureUnitCode %in% "mg/l") %>%
+  select("MonitoringLocationIdentifier",
+           "ActivityMediaSubdivisionName", 
+           "ActivityStartDate", 
+           "ActivityStartTime.Time",
+           "CharacteristicName",
+           "ResultSampleFractionText",
+           "ResultMeasureValue",
+           "ResultMeasure.MeasureUnitCode",
+           "USGSPCode") %>%
+  filter(ResultMeasure.MeasureUnitCode %in% c("mg/l", 'mg/l CaCO3', 'mg/l as Na')) %>%
+  distinct() %>%
   group_by(MonitoringLocationIdentifier) %>%
-  summarise(count=n(),
-            start=min(ActivityStartDateTime),
-            end=max(ActivityStartDateTime),
-            max = max(ResultMeasureValue, na.rm = TRUE)) %>%
-  filter(count > 300) %>%
-  arrange(-count) %>%
   left_join(siteInfo, by = "MonitoringLocationIdentifier")
-
 
 
